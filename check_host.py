4d07b7b9302b28b1f59a3a068bb0c43a6619e74f
wangsit <62087152+Wangsit7@users.noreply.github.com>
2021-03-16 12:22:32 +0700

    Tugas PSJ

--- check_host.py
+++ check_host.py
@@ -3,7 +3,7 @@ import sys
 
 
 if(len(sys.argv) <= 1):
-    print("IP address belum diberikan")
+    print("IP address belum diberikan!")
 else:
     ping = ['ping', '-c1', sys.argv[1]]
     check = subprocess.check_output(ping)
