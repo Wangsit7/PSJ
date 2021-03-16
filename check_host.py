6ab3f57499332b9c2ecff50e8dbb5ff4d2f647c9
wangsit <62087152+Wangsit7@users.noreply.github.com>
2021-03-16 12:20:11 +0700

    Update check_host.py

--- check_host.py
+++ check_host.py
@@ -5,9 +5,9 @@ import sys
 if(len(sys.argv) <= 1):
     print("IP address belum diberikan")
 else:
-    cmd = ['ping', '-c1', sys.argv[1]]
-    res = subprocess.check_output(cmd)
-    if(res):
+    ping = ['ping', '-c1', sys.argv[1]]
+    check = subprocess.check_output(ping)
+    if(check):
         print('UP')
     else:
         print('DOWN')
